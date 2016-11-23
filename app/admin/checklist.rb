76dad357415305a660ca732aead1bf2d411680e8
Raghu Betina <raghu@firstdraft.com>
2016-11-23 22:37:30 +0000

    Whitelist columns in Checklist for ActiveAdmin

--- app/admin/checklist.rb
+++ app/admin/checklist.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Checklist do
 
+ permit_params :trip_id, :name
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
